84d5c64105205912da380c42fc9eef6f4a1336dc
Davi <Daviemailfalso@teste.com>
2026-03-12 21:34:17 -0300

    Atualização aula 3

---
+++ python/loops.py
@@ -0,0 +1,65 @@
+# WHILE 
+
+# cont = 0
+
+# while True:
+#     cont = cont + 1
+#     print(cont)
+#     if cont >= 100:
+#         break
+
+# sair = 'não'
+# while 'sair' != 'sim':
+#     print ('0 - cadastrar')
+#     print ('1 - editar')
+#     print ('2 - excluir')
+
+
+
+    # opcao = int(input('O que deseja fazer?'))
+
+    # match opcao:
+    #     case 0:
+    #         print('voce escolheu cadastrar')
+    #         sair = input('Deseja Sair? Sim ou Não?')
+    #     case 1:
+    #         print('voce escolheu editar')
+    #         sair = input('Deseja Sair? Sim ou Não?')
+    #     case 2:
+    #         print('voce escolheu excluir')
+    #         sair = input('Deseja Sair? Sim ou Não?')
+    #     case _:
+    #         print('Invalido')
+    #         sair = input('Deseja Sair? Sim ou Não?')
+    
+
+# FOR para Repetição pre definido de vezes  
+  
+
+# for numero in range(1,11):
+#     print(numero)
+
+# percorrendo Listas
+
+# frutas = ['Uva','Maça', 'Melão', 'Kiwi']
+
+# for fruta in frutas:
+#     print(fruta)
+
+
+nome = 'Davi'
+
+for letra in nome:
+    print(letra)
+
+
+
+
+
+
+
+
+
+
+
+
